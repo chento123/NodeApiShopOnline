06b8883d458975db3f6d216d6effeab0b38b838c
chento123 <eksreng@gmail.com>
2022-10-31 22:24:31 +0700

    ApiOnlineShop

--- app/config/db.connect.js
+++ app/config/db.connect.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "db_shop_01",
+    host: "bs6yrnw4qi2chbktjh76-mysql.services.clever-cloud.com",
+    user: "uwhb8avxg6alt0e3",
+    password: "bs6yrnw4qi2chbktjh76",
+    database: "bs6yrnw4qi2chbktjh76",
 });
 db.connect();
 module.exports = db;
