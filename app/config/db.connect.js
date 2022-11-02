dd03b146425e631bc2ce8d37b6cd60c45bbc4812
chento123 <eksreng@gmail.com>
2022-11-02 13:53:40 +0700

    NodeApiShopOnline

--- app/config/db.connect.js
+++ app/config/db.connect.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "db_shop_01",
+    host: "b7liytagts1pagus0vyk-mysql.services.clever-cloud.com",
+    user: "u5b7wdovoxpvnw9m",
+    password: "ia7uAyrIm0laju5SkrEy",
+    database: "b7liytagts1pagus0vyk",
 });
 db.connect();
 module.exports = db;
